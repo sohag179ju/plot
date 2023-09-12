1126d11328967b972f8d33fec0e84c8615538c2a
sohag179ju <128798972+sohag179ju@users.noreply.github.com>
2023-09-12 21:40:08 +0600

    Update plot_3.R

--- plot_3.R
+++ plot_3.R
@@ -19,4 +19,4 @@ with(powerConsum, lines(DateTime, Sub_metering_2, col = "red"))
 with(powerConsum, lines(DateTime, Sub_metering_3, col = "blue"))
 legend("topright",lty = 1, col=c("black","red","blue"),
        legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
-dev.copy(device = png, width = 480, height = 480, file = "Plot2.png")
+dev.copy(device = png, width = 480, height = 480, file = "Plot3.png")
